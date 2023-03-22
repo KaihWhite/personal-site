9b40d11fbe227cac20e7cf17f4e7db76878dcd49
Kaih White <53894542+KaihWhite@users.noreply.github.com>
2023-03-22 12:41:27 -0700

    added website links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,10 @@ import styles from '../Footer.module.css';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <a href="https://www.kaihwhite.com" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/KaihWhite" target="_blank" rel="noopener noreferrer">
         <button className={styles.footerButton}>Visit Example Site</button>
       </a>
-      <a href="https://www.another-example.com" target="_blank" rel="noopener noreferrer">
+      <a href="hhttps://www.linkedin.com/in/kaihwhite/" target="_blank" rel="noopener noreferrer">
         <button className={styles.footerButton}>Visit Another Example Site</button>
       </a>
     </footer>
