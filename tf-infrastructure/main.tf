a35e12ad76e1007d7694418a86b141fdd69fbecc
Kaih White <53894542+KaihWhite@users.noreply.github.com>
2023-03-25 23:55:41 -0700

    updated build spec

--- tf-infrastructure/main.tf
+++ tf-infrastructure/main.tf
@@ -34,23 +34,24 @@ resource "aws_amplify_app" "personal-site" {
 
   # The default build_spec added by the Amplify Console for React.
   build_spec = <<-EOT
-    version: 0.1
-    frontend:
-      phases:
-        preBuild:
-          commands:
-            - yarn install
-        build:
-          commands:
-            - yarn run build
-      artifacts:
-        baseDirectory: build
-        files:
-          - '**/*'
-      cache:
-        paths:
-          - node_modules/**/*
-  EOT
+  version: 0.1
+  frontend:
+    phases:
+      preBuild:
+        commands:
+          - yarn install
+      build:
+        commands:
+          - yarn build
+    artifacts:
+      baseDirectory: .next
+      files:
+        - '**/*'
+    cache:
+      paths:
+        - node_modules/**/*
+        - .next/cache/**/*
+EOT
 
   # The default rewrites and redirects added by the Amplify Console.
   custom_rule {
