b1cef2b38ec99271c157e2d2e4b0d3243670dc53
Kaih White <kaihgwhite@outlook.com>
2023-11-27 13:59:07 -0800

    animated rotating cube wirefram in background and animation on my name on site homepage.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,77 @@
 // pages/index.js
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import Playground from '../components/Playground';
 import React from 'react';
-// TODO: incorperate framer motion or delete from project
+import { motion } from 'framer-motion';
+import { Canvas, useFrame } from '@react-three/fiber';
+
+function Box() {
+  const meshRef = React.useRef();
+
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.0025;
+      meshRef.current.rotation.y += 0.005;
+    }
+  });
+
+  return (
+    <mesh ref={meshRef} position={[0, 0, 0]}>
+      <boxBufferGeometry geometry='geometry' args={[3, 3, 3]}/>
+      <meshStandardMaterial wireframe color="white"/>
+    </mesh>
+  );
+}
 
 // Home page
 export default function Home() {
+  const AnimatedName = ({ name }) => {
+    return (
+      <div style={{ display: 'inline-block' }}>
+        {name.split('').map((letter, index) => (
+          <motion.span
+            key={index}
+            animate={{
+              y: [0, -20, 0],
+              transition: {
+                duration: 0.5,
+                ease: "easeInOut",
+                repeat: Infinity,
+                repeatDelay: 1.0,
+                delay: index * 0.1 // Staggered delay for each letter
+              }
+            }}
+            style={{ display: 'inline-block', marginRight: '2px' }}
+          >
+            {letter}
+          </motion.span>
+        ))}
+      </div>
+    );
+  };
+  
   return (
     <div className={styles.home}>
-      <Playground />
       <Head>
-        <title>Homepage</title>
+        <title>Welcome</title>
         <meta name="description" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/PrettyIcon.png" />
       </Head>
 
-      <header className={styles.banner}>
-        <h1>Kaih White</h1>
-      </header>
-      <div className={styles.container}>
-        <Footer />
+      <div className={styles.cubeContainer}>
+        <Canvas >
+          <ambientLight />
+          <Box />
+        </Canvas>
+      </div>
+
+      <div className={styles.intro}>
+        <div style={{ fontSize: '48px', fontWeight: 'bold', position: 'relative', top: '0px' }}>
+          Hello there,
+        </div>
+        <div style={{ fontSize: '36px', fontWeight: 'bold', position: 'relative', top: '0px', left: '300px' }}>
+          my name is <AnimatedName name="Kaih White" />
+        </div>
       </div>
     </div>
   );
