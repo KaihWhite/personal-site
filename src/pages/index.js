c017149dceeb160abf7d8468a9893c018fdde4c5
Kaih White <53894542+KaihWhite@users.noreply.github.com>
2023-11-27 16:43:00 -0800

    integrating framer motion for scroll animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,10 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import React from 'react';
+import { motion as m3 } from 'framer-motion-3d';
 import { motion } from 'framer-motion';
 import { Canvas, useFrame } from '@react-three/fiber';
+
 
 function Box() {
   const meshRef = React.useRef();
@@ -23,6 +25,22 @@ function Box() {
   );
 }
 
+
+// Intro
+function intro() {
+  return (
+    <div className={styles.intro}>
+      <div style={{ fontSize: '48px', fontWeight: 'bold', position: 'relative', top: '0px', right: '25px' }}>
+        Hello there,
+      </div>
+      <div style={{ fontSize: '36px', fontWeight: 'bold', position: 'relative', top: '0px', left: '300px' }}>
+        my name is <AnimatedName name="Kaih White" />
+      </div>
+    </div>
+  );
+}
+
+
 // Home page
 export default function Home() {
   const AnimatedName = ({ name }) => {
@@ -37,7 +55,7 @@ export default function Home() {
                 duration: 0.5,
                 ease: "easeInOut",
                 repeat: Infinity,
-                repeatDelay: 1.0,
+                repeatDelay: 2.0,
                 delay: index * 0.1 // Staggered delay for each letter
               }
             }}
@@ -65,8 +83,12 @@ export default function Home() {
         </Canvas>
       </div>
 
+      <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>
+
+      </motion.div> 
+
       <div className={styles.intro}>
-        <div style={{ fontSize: '48px', fontWeight: 'bold', position: 'relative', top: '0px' }}>
+        <div style={{ fontSize: '48px', fontWeight: 'bold', position: 'relative', top: '0px', right: '25px' }}>
           Hello there,
         </div>
         <div style={{ fontSize: '36px', fontWeight: 'bold', position: 'relative', top: '0px', left: '300px' }}>
