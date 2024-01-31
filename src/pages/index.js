69ed91fbe4498fab12787dadafaed3bc13210936
Kaih White <kaihgwhite@outlook.com>
2024-01-30 20:52:53 -0800

    added margins to links to space them

--- src/pages/index.js
+++ src/pages/index.js
@@ -65,9 +65,9 @@ function Box() {
 
 
 // Component to create a link with an icon
-function IconLink({ href, iconPath }) {
+function IconLink({ href, iconPath, margin}) {
   return (
-    <a href={href} target="_blank" rel="noopener noreferrer" style={{ display: 'inline-block' }}>
+    <a href={href} target="_blank" rel="noopener noreferrer" style={{ display: 'inline-block', margin: margin || '20px'}}>
       <img src={iconPath} alt="icon" style={{ width: '30px', height: '30px' }} />
     </a>
   );
