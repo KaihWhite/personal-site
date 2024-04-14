dcb6ca023002c911b259b869a25c7ff13f4725e0
Kaih White <kaihgwhite@outlook.com>
2024-04-14 13:57:52 -0700

    updated metadata for site, will need to ask Bing to recrawl my site to see this in search results

--- src/pages/index.js
+++ src/pages/index.js
@@ -116,8 +116,8 @@ export default function Home() {
     <div className={styles.home}>
       <Head>
         <title>Meet Kaih White</title>
-        {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
-        <meta name="description" content="Hello there! Welcome to my site." />
+        <meta name="description" content="Hello there! Welcome to Kaih's site." />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/PrettyIcon.png" />
       </Head>
 
