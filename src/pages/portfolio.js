dfd42ecce536bc6b8971363a3aa5694a2386c3af
Kaih White <kaihgwhite@outlook.com>
2024-07-31 01:56:21 -0700

    Simple, yet presentable portfolio section

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -24,21 +24,21 @@ export default function Portfolio() {
         <div style={{position: 'relative', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', paddingTop: '10%'}}>
             <h1 style={{ fontSize: '48px', fontWeight: 'bold', position: 'relative', top: '0px', paddingBottom: '5%'}}>Portfolio</h1>
             <Project 
-                title="Start Your Engine"
+                title="Lead Graphics Engineer"
                 description="Created a game engine from the ground up using C++ and OpenGL"
-                imgPath="https://via.placeholder.com/150"
+                imgPath="Start_Your_Engine_Logo.jpeg"
                 link="https://github.com/KaihWhite/Start-Your-Engine" />
                 
             <Project
-                title="Speedy Legal"
+                title="Lead Fullstack Engineer"
                 description="Created on-demand legal services app using React Native to connect clients with lawyers anywhere in the US."
-                imgPath="https://via.placeholder.com/150"
+                imgPath="SpeedyLegalLogo.png"
                 link="https://www.google.com" />
 
             <Project
-                title="Amazon Web Services internship"
+                title="Software Development Engineer Intern"
                 description="Worked as a software development engineer intern on the AWS Customer Experience team."
-                imgPath="https://via.placeholder.com/150"
+                imgPath="AWS_logo.png"
                 link="https://www.google.com" />
         </div>
     );
