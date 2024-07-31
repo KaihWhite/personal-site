18b1ab51daf7578a7f45b9fa9c0fe6dfb75e5081
Kaih White <53894542+KaihWhite@users.noreply.github.com>
2024-07-30 17:15:55 -0700

    edited portfolio section to look aesthetically pleasing

--- src/pages/index.js
+++ src/pages/index.js
@@ -133,7 +133,7 @@ export default function Home() {
         </Canvas>
       </div>
 
-      <div style={{position: 'relative', display: 'flex', paddingBottom: '50%', paddingTop: '25%', justifyContent:'center', alignItems:'center', overflow: 'hidden'}}>
+      <div style={{position: 'relative', display: 'flex', paddingBottom: '25%', paddingTop: '25%', justifyContent:'center', alignItems:'center', overflow: 'hidden'}}>
         <Intro />
       </div>
 
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -7,18 +7,22 @@ function Project({ title, description, imgPath, link }) {
   }
 
     return (
-        <div>
-            <h2>{title}</h2>
-            <p>{description}</p>
-            <img src={imgPath} alt="project" onClick={openLink}/>
+        <div style={{paddingBottom: '2%'}}>
+            <h2 style={{justifyContent: 'center', alignItems: 'center'}}>{title}</h2>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'row'}}>
+              <img src={imgPath} alt="project" onClick={openLink}/>
+              <div style={{paddingLeft: '1%', width: '300px', height: '200px', paddingTop: '15%'}}>
+                <p style={{}}>{description}</p>
+              </div>
+            </div>
         </div>
     );
 }
 
 export default function Portfolio() {
     return (
-        <div style={{position: 'relative', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
-            <h1>Portfolio</h1>
+        <div style={{position: 'relative', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', paddingTop: '10%'}}>
+            <h1 style={{ fontSize: '48px', fontWeight: 'bold', position: 'relative', top: '0px', paddingBottom: '5%'}}>Portfolio</h1>
             <Project 
                 title="Start Your Engine"
                 description="Created a game engine from the ground up using C++ and OpenGL"
