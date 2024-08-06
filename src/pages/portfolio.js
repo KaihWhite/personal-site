44be9e08a3d6997087c473183be025ad7a8d72ab
Kaih White <kaihgwhite@outlook.com>
2024-08-05 21:09:58 -0700

    added portfolio directory to contain documentation of my various projects.

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -4,7 +4,7 @@ import Menu from '../components/Menu';
 function Project({ description, imgPath, link }) {
   
       return (
-          <a href={link} style={{paddingTop: '64px'}}>
+          <a href={link} style={{paddingTop: '64px', border: '2px solid white', borderRadius: '10px'}}>
               <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
                 <img src={imgPath} alt="project" style={{width: '75vw', maxWidth: '500px', height: 'auto'}}/>
                 <div style={{maxWidth: '1000px', height: 'auto', paddingTop: '1%'}}>
@@ -17,11 +17,11 @@ function Project({ description, imgPath, link }) {
 
 export default function Portfolio() {
     return (
-        <div>
+        <div style={{background: 'solid black', minHeight: '100vh'}}>
             <Menu />
             <div style={{paddingTop: '64px', width: '100%', alignItems: 'center', position: 'relative'}}>
                 <h1 style={{textAlign: 'center', position: 'relative'}}>Portfolio</h1>
-                <p style={{fontSize: '20px',textAlign: 'center', position: 'relative', maxWidth: '1000px', margin: 'auto', padding: '16px 5% 0 5%'}}>Click on a project to see documentation of my development process</p>
+                <p style={{fontSize: '20px', textAlign: 'center', position: 'relative', maxWidth: '1000px', margin: 'auto', padding: '16px 5% 0 5%'}}>Click on a project to see documentation of my development process</p>
             </div>
 
             <div style={{paddingTop: '64px', width: '100%', alignItems: 'center', position: 'relative', paddingLeft: '5%', paddingRight: '5%', display: 'flex', flexDirection: 'column'}}>
---
+++ src/pages/portfolio/start_your_engine.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StartYourEngine = () => {
+    return (
+        <div>
+            <h1>Start Your Engine</h1>
+            {/* Add your content here */}
+        </div>
+    );
+};
+
+export default StartYourEngine;
