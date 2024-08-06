ee8b24dfdae682e9677c3e51617073487bea0b2c
Kaih White <kaihgwhite@outlook.com>
2024-08-05 18:13:32 -0700

    started development of portfolio page;

---
+++ src/pages/portfolio.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Menu from '../components/Menu';
+
+function Project({ description, imgPath, link }) {
+  
+      return (
+          <a href={link} style={{paddingTop: '64px'}}>
+              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <img src={imgPath} alt="project" style={{width: '75vw', maxWidth: '500px', height: 'auto'}}/>
+                <div style={{maxWidth: '1000px', height: 'auto', paddingTop: '1%'}}>
+                  <p style={{fontSize: '16px', textAlign: 'center'}}>{description}</p>
+                </div>
+              </div>
+          </a>
+      );
+  }
+
+export default function Portfolio() {
+    return (
+        <div>
+            <Menu />
+            <div style={{paddingTop: '64px', width: '100%', alignItems: 'center', position: 'relative'}}>
+                <h1 style={{textAlign: 'center', position: 'relative'}}>Portfolio</h1>
+                <p style={{fontSize: '20px',textAlign: 'center', position: 'relative', maxWidth: '1000px', margin: 'auto', padding: '16px 5% 0 5%'}}>Click on a project to see documentation of my development process</p>
+            </div>
+
+            <div style={{paddingTop: '64px', width: '100%', alignItems: 'center', position: 'relative', paddingLeft: '5%', paddingRight: '5%', display: 'flex', flexDirection: 'column'}}>
+                <Project 
+                    description="Relevant skills: C++, OpenGL, Graphics Engineering, Game Engine Development, Software Architecture, Hardware Optimization, Project Management"
+                    imgPath="Start_Your_Engine_Logo.jpeg"
+                    link="https://github.com/KaihWhite/Start-Your-Engine" />
+            </div>
+        </div>
+    );
+}
--- src/pages/work_experience.js
+++ src/pages/work_experience.js
@@ -1,10 +1,6 @@
 import React from 'react';
 
-function Project({ title, description, imgPath, link }) {
-
-  function openLink() {
-    window.open(link, '_blank');
-  }
+function Project({ title, description, imgPath }) {
 
     return (
         <div style={{paddingTop: '64px'}}>
@@ -21,29 +17,26 @@ function Project({ title, description, imgPath, link }) {
 
 export default function WorkExperience() {
     return (
-        <div style={{position: 'relative', display: 'flex', flexDirection: 'column', paddingTop: '5%', alignItems: 'center', padding: '5%'}}>
+        <div style={{position: 'relative', display: 'flex', flexDirection: 'column', paddingTop: '64px', alignItems: 'center', padding: '0 5% 0 5%'}}>
           <div style={{maxWidth: '1000px'}}>
             <h1 style={{ fontSize: '48px', fontWeight: 'bold', position: 'relative', top: '0px', textAlign: 'center'}}>Work Experience</h1>
             <Project 
                 title="Lead Engineer"
                 description="Created a game engine from the ground up using C++ and OpenGL while acting as the lead engineer for a team of 3.
                 Greatly improved my understanding of graphics programming and optimizing for hardware."
-                imgPath="Start_Your_Engine_Logo.jpeg"
-                link="https://github.com/KaihWhite/Start-Your-Engine" />
+                imgPath="Start_Your_Engine_Logo.jpeg" />
                 
             <Project
                 title="Lead Fullstack Engineer"
                 description="Created an on-demand legal service web app to connect clients with lawyers anywhere in the US via video call.
                 Acted as lead engineer for a team of 7 and used the AWS CDK to design a serverless architecture using Python and React."
-                imgPath="SpeedyLegalLogo.png"
-                link="https://www.google.com" />
+                imgPath="SpeedyLegalLogo.png" />
 
             <Project
                 title="Software Development Engineer Intern"
                 description="Interned as a software development engineer on the AWS Customer Experience team and learned how to build cloud-native applications using AWS services.
                 Specifcally, I worked on migrating a legacy AWS feature to a serverless architecture using the AWS CDK and Javascript."
-                imgPath="AWS_logo.png"
-                link="https://www.google.com" />
+                imgPath="AWS_logo.png" />
           </div>
         </div>
     );
